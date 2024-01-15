9df314dd71c0d54aedba5da92ec7207d97f5004e
EC2 Default User <ec2-user@ip-172-31-41-43.ap-northeast-1.compute.internal>
2024-01-15 11:40:45 +0000

    favorite update

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,13 +3,13 @@ class Public::FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_player.favorites.new(post_id: post.id)
     favorite.save
-    redirect_to public_posts_path
+    redirect_to public_post_path(post)
   end
 
   def destroy
     post = Post.find(params[:post_id])
     favorite = current_player.favorites.find_by(post_id: post.id)
     favorite.destroy
-    redirect_to public_posts_path
+    redirect_to public_post_path(post)
   end
 end
